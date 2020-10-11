9d50cb4c8dc29a623bdb623a1dd03e86f05b92ca
Sushmitha A T <48562432+sushmithaat@users.noreply.github.com>
2020-10-11 14:10:16 +0530

    Add files via upload

---
+++ ImageToPencilSketch/demo.py
@@ -0,0 +1,27 @@
+import numpy as np
+import imageio
+import scipy.ndimage
+import cv2
+
+
+def greyscale(rgb):
+     return np.dot(rgb[...,:3],[0.299,0.587,0.114])
+
+def dodge(front,back):
+    result=front*255/(255-back)
+    result[result>255]=255
+    result[back==255]=255
+    return result.astype('uint8')
+
+img="photo.jpg"
+s=imageio.imread(img)
+g=greyscale(s)
+i=255-g
+
+
+b=scipy.ndimage.filters.gaussian_filter(i,sigma=10)
+r=dodge(b,g)
+
+
+
+cv2.imwrite('1.png',r)
